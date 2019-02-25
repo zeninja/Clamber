ca18457ae3734b2f769b6d47ef78259c51493a16
Unknown <aea313@nyu.edu>
2019-02-25 17:46:02 -0500

    convert to gyro controls (doesn't work in build)
    - also evenly spaces holds vertically now i think?
    (would be nice to go back to the old version and test it out)

--- Assets/Scripts/HoldSpawner.cs
+++ Assets/Scripts/HoldSpawner.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 
 public class HoldSpawner : MonoBehaviour {
 
-
 	public int numHolds = 15;
 	public Hold holdPrefab;
-
 	public Vector2 startHoldPos = Vector2.zero;
-
-	Vector2 lastHoldPos;
 	Vector2 nextHoldPos;
-
-	public float holdDistance;
-	// public bool keepHoldsOnScreen = true;
 	
 	void Start() {
 		nextHoldPos = startHoldPos;
@@ -23,24 +16,13 @@ public class HoldSpawner : MonoBehaviour {
 		spreadAngle = maxSpreadAngle;
 	}
 
-
 	IEnumerator SpawnHolds() {
 		Vector2 nextHoldVector = Vector2.up;
 
 		for(int i = 0; i < numHolds; i++) {
 			Hold newHold = Instantiate(holdPrefab, nextHoldPos, Quaternion.identity);
-
 			nextHoldVector = GetNextHoldVector();
-
-			// if(keepHoldsOnScreen) {
-			// 	while (Mathf.Abs(nextHoldPos.x + nextHoldVector.x) > ScreenInfo.w) {
-			// 		nextHoldVector = GetNextHoldVector();
-			// 		yield return null;
-			// 	}
-			// }
-
 			nextHoldPos += nextHoldVector;
-			// yield return new WaitForSeconds(.1f);
 			yield return null;
 
 		}
@@ -50,7 +32,7 @@ public class HoldSpawner : MonoBehaviour {
 	public float distanceToNextHold = 2;
 
 	public float maxSpreadAngle = 45;
-
+  
 	public static float spreadAngle;
 
 	Vector2 GetNextHoldVector() {
@@ -60,7 +42,7 @@ public class HoldSpawner : MonoBehaviour {
 			rotation = Quaternion.AngleAxis(Random.Range(-maxSpreadAngle, maxSpreadAngle), Vector3.forward);
 		}
 
-		Vector2 nextHoldVector = rotation * Vector2.up * distanceToNextHold;
+		Vector2 nextHoldVector = new Vector2((rotation * Vector2.up * distanceToNextHold).x, distanceToNextHold);
 		return  nextHoldVector;
 	}
 }
