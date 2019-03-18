f790b6d8a84b466a5fa08831726cb3e6cb9fa613
Adnan Agha <aea313@nyu.edu>
2019-03-17 22:58:06 -0400

    Auto stash before merge of "dev" and "origin/dev"

--- Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+// using UnityEngine;
 using UnityEngine.PostProcessing;
 
 namespace UnityEditor.PostProcessing
@@ -6,19 +6,19 @@ namespace UnityEditor.PostProcessing
     [CustomPropertyDrawer(typeof(MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        public override void OnGUI(UnityEngine.Rect position, SerializedProperty property, UnityEngine.GUIContent label)
         {
             MinAttribute attribute = (MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
                 int v = EditorGUI.IntField(position, label, property.intValue);
-                property.intValue = (int)Mathf.Max(v, attribute.min);
+                property.intValue = (int)UnityEngine.Mathf.Max(v, attribute.min);
             }
             else if (property.propertyType == SerializedPropertyType.Float)
             {
                 float v = EditorGUI.FloatField(position, label, property.floatValue);
-                property.floatValue = Mathf.Max(v, attribute.min);
+                property.floatValue = UnityEngine.Mathf.Max(v, attribute.min);
             }
             else
             {
