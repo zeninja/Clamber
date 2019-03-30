58e7720c76a90f2dbc5428dfbc3ae26028c22bca
Adnan Agha <aea313@nyu.edu>
2019-03-30 17:55:32 -0400

    start text system

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,7 +5,30 @@ using TMPro;
 
 public class TextController : MonoBehaviour
 {
-    public void ShowText() {
 
+    public string[] instructions;
+
+    bool inputReceived = false;
+
+    int instructionIndex = 0;
+
+    public void IncrementIndex() {
+        instructionIndex = (instructionIndex + 1) % instructions.Length;
+    }
+
+    TextMeshProUGUI tmp;
+    public float timeBetweenLetters = .0125f;
+
+    IEnumerator ShowText() {
+        string targetString = instructions[instructionIndex];
+        char[] chars = targetString.ToCharArray();
+        string showString = "";
+
+        for(int i = 0; i < targetString.Length; i++) 
+        {
+            showString += chars[i];
+            tmp.text = showString;
+            yield return new WaitForSeconds(timeBetweenLetters);
+        }
     }
 }
